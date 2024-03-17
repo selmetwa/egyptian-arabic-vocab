76115dcdeea95c268814d24e8c41d3fcd3394df5
Sherif Elmetwally <sge5@njit.edu>
2024-03-17 18:20:25 -0400

    update endpoints

---
+++ README.md
@@ -0,0 +1,16 @@
+# Egyptian Arabic Vocabulary API
+
+[Words scraped from Desert Sky](https://arabic.desert-sky.net/vocab.html)
+
+## Sections
+Sections include both Egyptian and Standard Arabic, as well as the respective transliteration
+
+- [animals](/vocab/animals)
+- [colors](/vocab/colors)
+- [numbers](/vocab/numbers)
+
+## API
+Use the following endpoint to get the vocabulary for a section:
+
+GET /vocab/:section
+Replace `:section` with the section name (`/vocab/animals`)
---
+++ src/index.tsx
@@ -0,0 +1,83 @@
+import { serve } from "@hono/node-server";
+import fs from "fs";
+import { Hono } from "hono";
+import path from "path";
+import { css, Style } from "hono/css";
+import { sections } from "./constants/sections";
+const app = new Hono();
+
+app.get("/", (c) => {
+  const wrapper = css`
+    max-width: 1200px;
+    padding: 20px 20px;
+    margin: 0 20px;
+    display: flex;
+    flex-direction: column;
+    border: 4px solid #000;
+    box-shadow: 10px 10px 0 rgba(0, 0, 0, 1);
+    font-family: Arial, sans-serif;
+  `
+
+  return c.html(
+    <html>
+      <head>
+        <Style />
+      </head>
+      <body>
+        <main class={wrapper}>
+          <h1>Egyptian Arabic Vocabulary API</h1>
+          <a href="https://arabic.desert-sky.net/vocab.html" target="_blank">Words scraped from Desert Sky</a>
+          <section>
+            <h2>Sections</h2>
+            <p>Sections include both Egyptian and Standard arabic, as well as the respective transliteration</p>
+            <ul>
+              {sections.map((section) => (
+                <li>
+                  <a href={`/vocab/${section}`}>{section}</a>
+                </li>
+              ))}
+            </ul>
+          </section>
+          <section>
+            <h2>API</h2>
+            <p>Use the following endpoint to get the vocabulary for a section</p>
+            <pre>
+              <code>
+                GET /vocab/:section
+              </code>
+            </pre>
+            <p>Replace <code>:section</code> with the section name (<code>/vocab/animals</code>)</p>
+          </section>
+        </main>
+      </body>
+    </html>
+  )
+});
+
+app.get("/vocab/:section", async (c) => {
+  const section = c.req.param("section");
+
+  const filePath = path.join("json", `${section}.json`);
+
+  try {
+    const data = await fs.promises.readFile(filePath, "utf8");
+    const jsonData = JSON.parse(data);
+
+    return c.json(jsonData);
+  } catch (error: any) {
+    if (error.code === "ENOENT") {
+      return c.text("File not found");
+    } else {
+      console.error("Error reading/parsing file:", error);
+      return c.text("Error reading/parsing file");
+    }
+  }
+});
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
